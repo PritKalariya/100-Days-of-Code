c9a350c317d17af79aca8e5c6a9aaa00d16f1f1d
Prit Kalariya <71484962+PritKalariya@users.noreply.github.com>
2021-02-01 13:01:42 +0530

    Day 1

---
+++ Day 1/Band Name Generator.py
@@ -0,0 +1,6 @@
+print("Welcome to Band Name Generator")
+
+city = input("What's the name of the City you grew up in? \n")
+pet = input("What's your pet's name? \n")
+
+print(f"Your Band Name could be {city} {pet}.")
