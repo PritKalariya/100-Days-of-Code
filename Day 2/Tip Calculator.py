cd9c43c311c5989c0bec5bbc9f05d171af3e7b1f
Prit Kalariya <kalariyaprit2000@gmail.com>
2021-02-02 15:13:37 +0530

    Day 2

--- Day 2/Tip Calculator.py
+++ Day 2/Tip Calculator.py
@@ -8,6 +8,7 @@ tip_percentage = tip / 100
 total_tip_amount = bill * tip_percentage
 total_bill_amount = bill + total_tip_amount
 bill_per_person = total_bill_amount / num
+
 # final_amount = round(bill_per_person, 2)
 final_amount = "{:.2f}".format(bill_per_person)
 
