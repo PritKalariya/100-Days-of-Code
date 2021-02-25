fa5abb6622767ec5d7e2e57db229af51a77f5971
Prit Kalariya <kalariyaprit2000@gmail.com>
2021-02-25 23:30:03 +0530

    Todo 2 completed.

--- Day 20 & 21/main.py
+++ Day 20 & 21/main.py
@@ -1,19 +1,38 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Python Snake Game")
+screen.tracer(0)
+
 
 #TODO1: Create the snake body
 starting_position = [(0, 0), (-20, 0), (-40, 0)]
 
+segments = []
+
 for position in starting_position:
     new_segment = Turtle(shape="square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(position)
+    segments.append(new_segment)
+
 
 #TODO2: Move the snake
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[seg_num].forward(20)
+
 
 #TODO3: Control the snake
 
