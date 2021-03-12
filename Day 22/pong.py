c113c1a624ec3a5a748667477927bfb72d6cdde7
Prit Kalariya <kalariyaprit2000@gmail.com>
2021-03-12 23:46:31 +0530

    Todo 2 & 3 completed.

--- Day 22/pong.py
+++ Day 22/pong.py
@@ -1,4 +1,5 @@
-from turtle import Screen
+from turtle import Screen, Turtle
+from paddle import Paddle
 
 
 #TODO1: Create the screen
@@ -6,11 +7,26 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong Game")
+screen.listen()
+screen.tracer(0) # Disableing the animations
 
 
 #TODO2: Create a moving paddle
+r_paddle = Paddle((350, 0))
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
 
 #TODO3: Create another moving paddle
+l_paddle = Paddle((-350, 0))
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
 
 #TODO4: Create the ball and make it move
 
