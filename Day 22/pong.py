996fef0c33e1e2e9cd8727c15405cd8d3093a08c
Prit Kalariya <kalariyaprit2000@gmail.com>
2021-03-12 22:49:34 +0530

    Todo 1 completed.

---
+++ Day 22/pong.py
@@ -0,0 +1,26 @@
+from turtle import Screen
+
+
+#TODO1: Create the screen
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong Game")
+
+
+#TODO2: Create a moving paddle
+
+#TODO3: Create another moving paddle
+
+#TODO4: Create the ball and make it move
+
+#TODO5: Detect collision with wall and bounce back
+
+#TODO6: Detect collision with paddle
+
+#TODO7: Detect when paddle misses the ball
+
+#TODO8: Keep scores
+
+
+screen.exitonclick()
