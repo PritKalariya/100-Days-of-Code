e8eb8a66ae96c7fcb1a2846284fe36822068f953
Prit Kalariya <kalariyaprit2000@gmail.com>
2021-03-28 23:38:40 +0530

    Day 25 completed.

---
+++ Day 25/US States Game/main.py
@@ -0,0 +1,38 @@
+import turtle
+import pandas as pd
+
+
+screen = turtle.Screen()
+screen.title("US States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+
+turtle.shape(image)
+
+
+data = pd.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States correct", prompt="Enter the name of the state:").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv("States_to_learn.csv")
+        break
+
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
