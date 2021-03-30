c97d8e297af923a6c75dcdfc69de8db33bbd4798
Prit Kalariya <kalariyaprit2000@gmail.com>
2021-03-30 15:45:27 +0530

    Refactoring

--- Day 25/US States Game/main.py
+++ Day 25/US States Game/main.py
@@ -19,10 +19,7 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States correct", prompt="Enter the name of the state:").title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pd.DataFrame(missing_states)
         new_data.to_csv("States_to_learn.csv")
         break
