f709e5ce842b80fa83d32713e56f2011bb6dd091
Prit Kalariya <kalariyaprit2000@gmail.com>
2021-04-05 22:36:16 +0530

    Error handling.

--- Day 26/main.py
+++ Day 26/main.py
@@ -9,6 +9,15 @@ phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry! Only enters in the alphabets please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
