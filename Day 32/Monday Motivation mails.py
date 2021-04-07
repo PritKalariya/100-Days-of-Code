f134ce035525bc6e5f3ac341aca9bbb7c0acfc99
Prit Kalariya <kalariyaprit2000@gmail.com>
2021-04-07 23:34:29 +0530

    Monday Motivation Email (automation).

---
+++ Day 32/Monday Motivation mails.py
@@ -0,0 +1,26 @@
+import smtplib
+import datetime as dt
+import random
+
+
+MY_EMAIL = "testing email address"
+MY_PASSWORD = "password"
+
+
+now = dt.datetime.now()
+weekday = now.weekday()
+
+
+if weekday == 2:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=f"Subject: Monday Motivation\n\n{quote}"
+        )
