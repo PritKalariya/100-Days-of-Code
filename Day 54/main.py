4deb66bc41732fed6183cfafe6b1ac06c932a228
Prit Kalariya <kalariyaprit2000@gmail.com>
2021-06-07 19:57:40 +0530

    Extract URL parts and declaring variables.

--- Day 54/main.py
+++ Day 54/main.py
@@ -12,5 +12,16 @@ def hello_world():
 def say_bye():
     return "Bye"
 
+# Add a variable section in the URL
+# For more refer documentation https://flask.palletsprojects.com/en/1.1.x/quickstart/
+@app.route("/<name>/<int:number>")
+def greet(name, number):
+    return f"Grettings {name}!! You aare {number} years old."
+
+
 if __name__ == "__main__":
-    app.run()
+    # Debug=TRUE will
+    # 1. Activate the debugger
+    # 2. Activate the automatic reloader
+    # 3. Activates the debug mode on the flask application
+    app.run(debug=True)
