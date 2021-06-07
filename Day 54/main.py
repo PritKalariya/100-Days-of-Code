943614ca6be00c8926458e2107fd6ecee082a160
Prit Kalariya <kalariyaprit2000@gmail.com>
2021-06-07 22:54:03 +0530

    Using python decorators to style HTML elements.

--- Day 54/main.py
+++ Day 54/main.py
@@ -4,15 +4,27 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# Python decorators to stylr HTML elements
+def make_bold(func):
+    def wrapper_function():
+        return "<b>" + func() + "</b>"
+    return wrapper_function
+
+
+# Decorator
 @app.route('/')
 def hello_world():
     return '<h1 style="text-align: center">Hello, World!</h1>' \
         '<p>This is testing paragraph.</p><br>' \
         '<img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width=300>'
 
+
+# Applying the python decorators
 @app.route("/bye")
+@make_bold
 def say_bye():
     return "Bye"
+
 
 # Add a variable section in the URL
 # For more refer documentation https://flask.palletsprojects.com/en/1.1.x/quickstart/
