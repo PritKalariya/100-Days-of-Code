b681ffe85f0f93030cf8cc55f630334b7ebf66cc
Prit Kalariya <kalariyaprit2000@gmail.com>
2021-06-10 23:35:43 +0530

    Blog templating with jinja in flask application.

---
+++ Day 57/main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from post import Post
+import requests
+
+
+posts = requests.get("https://api.npoint.io/52d4ccdf40a952119443").json()
+post_objects = []
+for post in posts:
+    print(post)
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=post_objects)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
