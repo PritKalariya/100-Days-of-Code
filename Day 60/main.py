28718f7b6aa9a7b6eaa5f238972b82588cc59801
Prit Kalariya <kalariyaprit2000@gmail.com>
2021-06-30 23:25:09 +0530

    Designed the HTML form and app route.

---
+++ Day 60/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/login", methods=["POST"])
+def receive_data():
+    name = request.form["username"]
+    password = request.form["password"]
+    return f"<h1>Name: {name}, Password: {password}</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
