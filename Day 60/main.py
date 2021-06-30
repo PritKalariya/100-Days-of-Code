0e70a7eef1c01941a36a79da5234c6864b6989e7
Prit Kalariya <kalariyaprit2000@gmail.com>
2021-07-01 00:08:50 +0530

    Working Contact Form.

--- Day 60/main.py
+++ Day 60/main.py
@@ -1,4 +1,12 @@
 from flask import Flask, render_template, request
+import smtplib
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+MY_EMAIL = os.getenv("MY_EMAIL")
+MY_PASSWORD = os.getenv("MY_PASSWORD")
 
 
 app = Flask(__name__)
@@ -8,11 +16,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/login", methods=["POST"])
+@app.route("/form entry", methods=["GET", "POST"])
 def receive_data():
-    name = request.form["username"]
-    password = request.form["password"]
-    return f"<h1>Name: {name}, Password: {password}</h1>"
+    data = request.form
+    if request.method == "POST":
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            connection.sendmail(
+                from_addr=data["email"],
+                to_addrs=MY_EMAIL,
+                msg=f"Subject:New Review\n\nName: {data['firstName']} {data['lastName']}\nEmail: {data['email']}\nMessage: {data['massage']}."
+            )
+        return f"<h1>Message Sent Successfully</h1>"
+    else:
+        return render_template("index.html")
 
 
 if __name__ == '__main__':
