401273a9cc0520a0341225577f957a9b3e3fde75
Prit Kalariya <kalariyaprit2000@gmail.com>
2021-07-03 22:09:34 +0530

    Refactoring.

--- Day 61/main.py
+++ Day 61/main.py
@@ -1,24 +1,27 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email')
-    password = StringField('Password')
+    email = StringField(label='Email', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Log In')
 
 
 app = Flask(__name__)
-app.secret_key = ""
+app.secret_key = "URft9izjQ$R*96c"
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
+    login_form.validate_on_submit()
     return render_template("login.html", form=login_form)
 
 
