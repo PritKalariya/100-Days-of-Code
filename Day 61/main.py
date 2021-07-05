91f8bdded5f9c961c174b94683f2f010fc5ea558
Prit Kalariya <kalariyaprit2000@gmail.com>
2021-07-05 12:48:32 +0530

    Advance Form with Flask-WTForms.

--- Day 61/main.py
+++ Day 61/main.py
@@ -1,27 +1,34 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap
 
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
-    submit = SubmitField(label='Log In')
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label="Log In")
 
 
 app = Flask(__name__)
-app.secret_key = "URft9izjQ$R*96c"
+app.secret_key = "any-string-you-want-just-keep-it-secret"
+Bootstrap(app)
 
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route("/login", methods=['GET', 'POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
-    login_form.validate_on_submit()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
     return render_template("login.html", form=login_form)
 
 
